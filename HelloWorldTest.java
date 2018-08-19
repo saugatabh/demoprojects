edf0f830b857e5c312e276eb29758492cabea51a
Saugata <saugata16@gmail.com>
2018-08-19 15:53:59 +0530

    third commit

--- HelloWorldTest.java
+++ HelloWorldTest.java
@@ -1,4 +1,4 @@
-import static org.junit.Assert.*;
+import org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,10 +11,10 @@ public class HelloWorldTest {
 	public void setup() {
 		subject = new HelloWorld();
 	}
-	
+
 	@Test
 	public void testGetMessage() {
 		assertEquals("Hello World!", subject.getMessage(false));
 	}
-	
+
 }
